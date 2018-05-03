b84bb4275a51e7d157fd712060ae6b636024808a
tina tong <tinale0414@gmail.com>
2018-05-02 20:24:17 -0700

    updated

--- server.js
+++ server.js
@@ -1,47 +1,44 @@
-//Get the package
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
-//Initialize Express
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
 app.use(bodyParser.json());
 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
-
-// Reservation Data
-// ===========================================================
-
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/app", function (req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-});
-
-app.get("/app", function (req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
-});
-
-app.get("/view", function (req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-});
-
-
-app.get("/api/reservations", function (req, res) { // /api shows that we are in the data page
-    res.send(reservations);
-});
-
-app.post("/api/new", function (req, res) {
-    var newReservation = req.body;
-    reservations.push(newReservation);
-    res.json(newReservation);
+  console.log("App listening on PORT: " + PORT);
 });
